6009d591ddcd95e42e44153fd4be34c448533eba
PetrashU <petrashevichulyana@gmail.com>
2022-05-31 18:38:36 +0200

    Jeszcze komentarze do Edge

--- kod/src/main/java/graph/Edge.java
+++ kod/src/main/java/graph/Edge.java
@@ -4,9 +4,9 @@ public class Edge {
     public int fin;
     public double weight;
     public Edge(){}
-    public Edge(int fin, double weight){
-        this.fin = fin;
-        this.weight = weight;
+    public Edge(int fin, double weight){        //krawędź
+        this.fin = fin;                 //wierzchołek końcowy
+        this.weight = weight;           //waga
     }
 
 
